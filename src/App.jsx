e9ac99f2d59309a5121aa6afac973246accc1bc8
Naveen Verma <nverma2k@gmail.com>
2025-09-24 21:18:41 +0530

    Commit 5: Added logic to calculate the results and show it in table.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import Header from "./components/Header"
-import UserInput from "./components/UserInput"
+import { useState } from "react";
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 1000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    // console.log(event);
+    setUserInput((previousInput) => {
+      return {
+        ...previousInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
   return (
     <>
-    <Header />
+      <Header />
 
-    <UserInput />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {!inputIsValid && (
+        <p className="centre">Please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <Results userInput={userInput} />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
